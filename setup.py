2f14f797f382e44073d820972e0fa99c7f494bbf
bpuchala <bpuchala@users.noreply.github.com>
2023-08-13 00:41:02 -0400

    version 2.0a3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="libcasm-xtal",
-    version="2.0a2",
+    version="2.0a3",
     packages=["libcasm", "libcasm.xtal"],
     package_dir={"": "python"},
     cmake_install_dir="python/libcasm",
