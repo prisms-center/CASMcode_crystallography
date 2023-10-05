823a8be4eba84d906705831e89369f6d84772670
bpuchala <bpuchala@users.noreply.github.com>
2023-10-05 10:15:32 -0400

    Add mode option to Structure.from_poscar and Structure.from_poscar_str to specify whether the POSCAR should be read as atoms, molecules, or both

--- python/tests/test_structure.py
+++ python/tests/test_structure.py
@@ -153,6 +153,126 @@ def test_structure_to_poscar_str_3(example_structure_2):
     assert lines[7] == "Direct"
 
 
+def test_structure_from_poscar_str_1():
+    poscar_str = """test structure
+    1.0
+    4.0 0.0 0.0
+    0.0 4.0 0.0
+    0.0 0.0 4.0
+    A B 
+    1 3 
+    Cartesian
+    0.0 0.0 0.0 
+    0.5 0.5 0.5 
+    0.0 0.0 1.0 
+    0.5 0.5 1.5 
+    """
+    structure = xtal.Structure.from_poscar_str(poscar_str)
+
+    assert np.allclose(structure.lattice().column_vector_matrix(), np.eye(3) * 4.0)
+    assert len(structure.mol_type()) == 0
+    assert structure.mol_coordinate_frac().shape == (3, 0)
+    assert len(structure.mol_properties()) == 0
+    assert len(structure.atom_type()) == 4
+    assert structure.atom_coordinate_frac().shape == (3, 4)
+    assert len(structure.atom_properties()) == 0
+
+
+def test_structure_from_poscar_str_with_selectivedynamics_1():
+    poscar_str = """test structure
+    1.0
+    4.0 0.0 0.0
+    0.0 4.0 0.0
+    0.0 0.0 4.0
+    A B
+    1 3
+    Selective dynamics
+    Cartesian
+    0.0 0.0 0.0 T T T
+    0.5 0.5 0.5 T T T
+    0.0 0.0 1.0 F F F
+    0.5 0.5 1.5 T T T
+    """
+    # default, read as atoms
+    structure = xtal.Structure.from_poscar_str(poscar_str)
+
+    assert np.allclose(structure.lattice().column_vector_matrix(), np.eye(3) * 4.0)
+
+    assert len(structure.mol_type()) == 0
+    assert structure.mol_coordinate_frac().shape == (3, 0)
+    assert len(structure.mol_properties()) == 0
+
+    assert len(structure.atom_type()) == 4
+    assert structure.atom_coordinate_frac().shape == (3, 4)
+    assert len(structure.atom_properties()) == 1
+    assert "selectivedynamics" in structure.atom_properties()
+    assert structure.atom_properties()["selectivedynamics"].shape == (3, 4)
+
+
+def test_structure_from_poscar_str_with_selectivedynamics_2():
+    poscar_str = """test structure
+    1.0
+    4.0 0.0 0.0
+    0.0 4.0 0.0
+    0.0 0.0 4.0
+    A B
+    1 3
+    Selective dynamics
+    Cartesian
+    0.0 0.0 0.0 T T T
+    0.5 0.5 0.5 T T T
+    0.0 0.0 1.0 F F F
+    0.5 0.5 1.5 T T T
+    """
+    # default, read as molecules
+    structure = xtal.Structure.from_poscar_str(poscar_str, mode="molecules")
+
+    assert np.allclose(structure.lattice().column_vector_matrix(), np.eye(3) * 4.0)
+
+    assert len(structure.mol_type()) == 4
+    assert structure.mol_coordinate_frac().shape == (3, 4)
+    assert len(structure.mol_properties()) == 1
+    assert "selectivedynamics" in structure.mol_properties()
+    assert structure.mol_properties()["selectivedynamics"].shape == (3, 4)
+
+    assert len(structure.atom_type()) == 0
+    assert structure.atom_coordinate_frac().shape == (3, 0)
+    assert len(structure.atom_properties()) == 0
+
+
+def test_structure_from_poscar_str_with_selectivedynamics_3():
+    poscar_str = """test structure
+    1.0
+    4.0 0.0 0.0
+    0.0 4.0 0.0
+    0.0 0.0 4.0
+    A B
+    1 3
+    Selective dynamics
+    Cartesian
+    0.0 0.0 0.0 T T T
+    0.5 0.5 0.5 T T T
+    0.0 0.0 1.0 F F F
+    0.5 0.5 1.5 T T T
+    """
+    # default, read as atoms & molecules
+    structure = xtal.Structure.from_poscar_str(poscar_str, mode="both")
+
+    assert np.allclose(structure.lattice().column_vector_matrix(), np.eye(3) * 4.0)
+
+    assert len(structure.mol_type()) == 4
+    assert structure.mol_coordinate_frac().shape == (3, 4)
+    assert len(structure.mol_properties()) == 1
+    assert "selectivedynamics" in structure.mol_properties()
+    assert structure.mol_properties()["selectivedynamics"].shape == (3, 4)
+
+    assert len(structure.mol_type()) == 4
+    assert structure.mol_coordinate_frac().shape == (3, 4)
+    assert len(structure.mol_properties()) == 1
+    assert "selectivedynamics" in structure.mol_properties()
+    assert structure.mol_properties()["selectivedynamics"].shape == (3, 4)
+
+
 def test_copy_structure(example_structure_1):
     import copy
 
