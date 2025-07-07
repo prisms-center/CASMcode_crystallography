cdc523e8e8d178c90820caf3802329e0f33141fe
bpuchala <bpuchala@users.noreply.github.com>
2025-07-07 19:37:21 -0400

    version bump 2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="libcasm-xtal",
-    version="2.0.1",
+    version="2.1.0",
     packages=["libcasm", "libcasm.xtal"],
     package_dir={"": "python"},
     cmake_install_dir="python/libcasm",
